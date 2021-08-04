c05bee1a82ed494f3e4c636888545a2c50222d26
S-L-Murphey <smurphey1990@gmail.com>
2021-08-04 13:11:19 -0500

    Completed Chapter 8: Listing Events

--- levelup/urls.py
+++ levelup/urls.py
@@ -19,10 +19,12 @@ from levelupapi.views import register_user, login_user
 from rest_framework import routers
 from levelupapi.views.gametype import GameTypeView
 from levelupapi.views.game import GameView
+from levelupapi.views.event import EventView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
 router.register(r'games', GameView, 'game')
+router.register(r'events', EventView, 'event')
 
 urlpatterns = [
     path('', include(router.urls)),
